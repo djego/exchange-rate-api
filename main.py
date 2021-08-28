42c03cb85cf0fedc375cc7c9b73d11a9f79388c3
Diego Machaca <diego.machaca@gmail.com>
2021-08-28 02:19:58 -0500

    chore: use houses exchange rate extra data in json file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
@@ -6,10 +7,13 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
+houses_extra = {}
+with open('./houses.json') as jsonfile:
+    houses_extra = json.load(jsonfile)
 
 def _get_history(house):
     cur = mongo.db.dollar.find({"house": house}, {
-                                '_id': False, 'house': True, 'buy': True, 'sell': True, 'date': True})
+        '_id': False, 'house': True, 'buy': True, 'sell': True, 'date': True}).sort('date', -1)
     result = []
     for row in cur:
         result.append({
@@ -19,15 +23,20 @@ def _get_history(house):
     return result
 
 
-@app.route("/v1")
+@app.route("/v1/resume")
 def home():
-    cur_last = mongo.db.dollar.find().sort("date",-1).limit(4)
+    cur_last = mongo.db.dollar.find().sort(
+        [("date", -1)]).limit(4)
     last = []
     for row in cur_last:
         last.append({
-                "tienda": row.get("house"),
-                "compra": row.get("buy"),
-                "venta": row.get("sell")})
+            "tienda": row.get("house"),
+            "compra": float(row.get("buy")),
+            "venta": float(row.get("sell")),
+            "fecha": row.get("date"),
+            "url": houses_extra[row.get("house")]["url"],
+            "image": houses_extra[row.get("house")]["image"]
+        })
     return jsonify(last)
 
 
@@ -36,15 +45,18 @@ def tkambio():
     history = _get_history("Tkambio")
     return jsonify(history)
 
+
 @app.route("/v1/cambioseguro")
 def cambioseguro():
     history = _get_history("Cambioseguro")
     return jsonify(history)
 
+
 @app.route("/v1/rextie")
 def rextie():
     history = _get_history("Rextie")
     return jsonify(history)
+
 
 @app.route("/v1/kambista")
 def kambista():
